6d01e52336d002746f8576e85086f72eb2146292
Rsja12 <asocarras10@gmail.com>
2020-06-04 11:26:11 -0400

    add lifecycle methods

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export class ResourceList extends Component {
 
-    
+    state = { 
+        resources: []
+    }
+
+    async componentDidMount() {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+
+        this.setState({
+            resources: response.data
+        })
+    }
+
+    async componentDidUpdate(prevProps) {
+        if (prevProps.resource !== this.props.resource) {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+
+            this.setState({
+                resources: response.data
+            })
+        }
+    }
 
     render() {
+        console.log(this.props.resource)
         return (
             <div>
-                { this.props.resource }
+                { this.state.resources.length }
             </div>
         )
     }
